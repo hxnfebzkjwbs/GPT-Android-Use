abd6a07d9d89f64171dea807e3fba8db2ff59fe9
hxnfebzkjwbs <qazujmko@126.com>
2026-09-18 17:15:41 +0800

    Bump version to 0.4.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hxnfebzkjwbs.gptandroiduse"
         minSdk = 30
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.4.7"
+        versionCode = 12
+        versionName = "0.4.8"
     }
 
     buildFeatures { viewBinding = true }
