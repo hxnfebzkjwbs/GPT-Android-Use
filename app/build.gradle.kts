0bfad53ccadedfa364d044673999959ef4f376eb
hxnfebzkjwbs <qazujmko@126.com>
2026-09-18 10:55:21 +0800

    Bump version for external Google login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hxnfebzkjwbs.gptandroiduse"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
     }
 
     buildFeatures { viewBinding = true }
