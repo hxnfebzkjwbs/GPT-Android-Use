4494b5e4d1a335e3a580c99f867f57e23f515390
hxnfebzkjwbs <qazujmko@126.com>
2026-09-18 11:01:36 +0800

    Bump version for fixed signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hxnfebzkjwbs.gptandroiduse"
         minSdk = 30
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.4.2"
     }
 
     buildFeatures { viewBinding = true }
