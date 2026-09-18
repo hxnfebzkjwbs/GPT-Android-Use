9722e7a6f3c836dce45686c5c135fb1e35d52c56
hxnfebzkjwbs <qazujmko@126.com>
2026-09-18 18:00:44 +0800

    Release 0.5.0 obvious one-tap ADB button
    * Make one-tap ADB button obvious
    
    * Show app version on main screen
    
    * Bump version to 0.5.0
    
    * Build private-signed 0.5.0 APK
    
    * Document visible one-tap control
    
    * Fix version label build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hxnfebzkjwbs.gptandroiduse"
         minSdk = 30
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.4.9"
+        versionCode = 14
+        versionName = "0.5.0"
     }
 
     buildFeatures { viewBinding = true }
