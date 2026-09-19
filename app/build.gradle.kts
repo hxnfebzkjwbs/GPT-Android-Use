508242ff080da748fdf7f7b4659e6b61a09ff060
hxnfebzkjwbs <qazujmko@126.com>
2026-09-19 22:23:10 +0800

    Release 0.15.5 reliably embed uploaded PNG icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hxnfebzkjwbs.gptandroiduse"
         minSdk = 30
         targetSdk = 35
-        versionCode = 89
-        versionName = "0.15.4"
+        versionCode = 90
+        versionName = "0.15.5"
     }
 
     flavorDimensions += "controlMode"
